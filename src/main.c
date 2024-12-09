5b4d0821fcb96a476b5e75938546884842ebd93c
X1 <corloss@web.de>
2024-12-09 11:41:43 +0100

    works with instant SAADC start

--- src/main.c
+++ src/main.c
@@ -7,9 +7,12 @@
 LOG_MODULE_REGISTER(LOG);
 
 //  Declare variable used to keep track of which buffer was last assigned to the SAADC driver
-static uint8_t saadc_buffer_num = 0;
-static volatile uint16_t *saadc_results;
-static volatile bool saadc_done = false;
+uint8_t saadc_buffer_num = 0;
+volatile int16_t *saadc_results;
+volatile bool saadc_done = false;
+int32_t vddh_div5_raw    = 0;
+int32_t coil_voltage_raw = 0;
+int32_t coil_power_raw   = 0;
 
 void saadc_handler(nrfx_saadc_evt_t const *p_event)
 {
@@ -18,16 +21,8 @@ void saadc_handler(nrfx_saadc_evt_t const *p_event)
     {
 
     case NRFX_SAADC_EVT_DONE:
-
-        saadc_done = true;
-
-        int32_t current_value;
-
+        saadc_done    = true;
         saadc_results = p_event->data.done.p_buffer;
-
-        current_value = (int32_t)(((int16_t *)(p_event->data.done.p_buffer))[0]) * (int32_t)(((int16_t *)(p_event->data.done.p_buffer))[1]);
-
-        LOG_INF("SAADC buffer at 0x%x filled with result %d", (uint32_t)p_event->data.done.p_buffer, current_value);
         break;
 
     case NRFX_SAADC_EVT_BUF_REQ:
@@ -38,6 +33,7 @@ void saadc_handler(nrfx_saadc_evt_t const *p_event)
     case NRFX_SAADC_EVT_READY:
         // Buffer is ready, timer (and sampling) can be started
         timer_for_saadc_sampling_start();
+        nrf_saadc_task_trigger(NRF_SAADC, NRF_SAADC_TASK_SAMPLE); // needed to start first SAADC sampling instantly, then PPI handles it via Timer CC
         break;
 
     default:
@@ -58,31 +54,14 @@ int main(void)
         {
             saadc_done = false;
 
-            uint32_t start_cycles = k_cycle_get_32(); // Get start cycle count
+            vddh_div5_raw    = (int32_t)saadc_results[0];
+            coil_voltage_raw = (int32_t)saadc_results[1];
 
-            int32_t current_value = (int32_t)saadc_results[0] * (int32_t)saadc_results[1];
-            if (current_value > 200000)
-            {
-                for (size_t i = 0; i < 5000; i++)
-                {
-                    saadc_done = !saadc_done;
-                }
-            }
+            coil_power_raw = vddh_div5_raw * coil_voltage_raw;
 
-            uint32_t end_cycles = k_cycle_get_32(); // Get end cycle count
-
-            // Calculate elapsed cycles
-            uint32_t elapsed_cycles = end_cycles - start_cycles;
-
-            // Convert to time in nanoseconds (if needed)
-            uint64_t elapsed_ns = k_cyc_to_ns_floor64(elapsed_cycles);
-
-            LOG_ERR("current saadc bufffer address: 0x%x result %d ", (uint32_t)saadc_results, current_value);
-
-            LOG_WRN("Result: %d, Elapsed Cycles: %u, Elapsed Time: %llu ns\n",
-                    end_cycles, start_cycles, elapsed_ns);
+            LOG_ERR("current saadc bufffer address: 0x%x result %d ", (uint32_t)saadc_results, coil_power_raw);
         }
 
-        k_msleep(1000);
+        k_msleep(1);
     }
 }
