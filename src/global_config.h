4f2d71f07fbec5b79a082e46bbbec0715f0ab9fd
X1 <corloss@web.de>
2024-12-09 18:18:39 +0100

    went to maximal saadc cycles

--- src/global_config.h
+++ src/global_config.h
@@ -6,9 +6,10 @@
 #define RED_LED      NRF_GPIO_PIN_MAP(0, 13)
 #define POWER_MOSFET NRF_GPIO_PIN_MAP(0, 16)
 
-#define SAADC_SAMPLE_INTERVAL_US 1000000 // sample every 5 µs to get the max possible 200 kHz SAADC
-#define SAADC_BUF_SIZE           2       // at 200kHz it takes 20ms to fill the 4000 buffer
-
-#define TIME_TO_SYSTEM_OFF_S 30
+#define POWER_MOSFET_TURN_ON_TIME_US 10
+#define TURN_ON_INTERVAL_MS          100
+#define SAADC_SAMPLE_INTERVAL_US     20 // sample every 5 µs to get the max possible 200 kHz SAADC
+#define SAADC_BUF_SIZE               2       // at 200kHz it takes 20ms to fill the 4000 buffer
+#define TIME_TO_SYSTEM_OFF_S         30
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,41 +1,40 @@
 #include "error_handling.h"
+#include "global_config.h"
 #include "saadc.h"
 #include "timer_and_ppi.h"
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(VAPE);
-K_SEM_DEFINE(saadc_sem, 0, 1);
+K_SEM_DEFINE(saadc_done_sem, 0, 1);
+K_SEM_DEFINE(saadc_ready_sem, 0, 1);
 
 //  Declare variable used to keep track of which buffer was last assigned to the SAADC driver
 uint8_t saadc_buffer_num = 0;
 volatile int16_t *saadc_results;
-volatile bool saadc_done = false;
 int32_t vddh_div5_raw    = 0;
 int32_t coil_voltage_raw = 0;
 int32_t coil_power_raw   = 0;
 
 void saadc_handler(nrfx_saadc_evt_t const *p_event)
 {
-    LOG_INF("SAADC evt %d", p_event->type);
+    // LOG_INF("SAADC evt %d", p_event->type);
     switch (p_event->type)
     {
 
     case NRFX_SAADC_EVT_DONE:
         saadc_results = p_event->data.done.p_buffer;
-        k_sem_give(&saadc_sem);
+        k_sem_give(&saadc_done_sem);
         break;
 
     case NRFX_SAADC_EVT_BUF_REQ:
-        // set the already handled buffer of doubled buffer as new
+        // set alternating buffer for double buffering
         saadc_update_buffer((saadc_buffer_num++) % 2);
         break;
 
     case NRFX_SAADC_EVT_READY:
         // Buffer is ready, timer (and sampling) can be started
-        k_sem_give(&saadc_sem);
-        // timer_for_saadc_sampling_start();
-        // nrf_saadc_task_trigger(NRF_SAADC, NRF_SAADC_TASK_SAMPLE); // needed to start first SAADC sampling instantly, then PPI handles it via Timer CC
+        k_sem_give(&saadc_ready_sem);
         break;
 
     default:
@@ -49,7 +48,7 @@ void start_heating(void)
     nrf_gpio_pin_set(POWER_MOSFET); // => ON
     nrf_gpio_pin_clear(RED_LED);    // => ON
 
-    k_usleep(10);
+    k_usleep(10); // wait until POWER_MOSFET is fully turned on
 
     timer_for_saadc_sampling_start();
     nrf_saadc_task_trigger(NRF_SAADC, NRF_SAADC_TASK_SAMPLE); // needed to start first SAADC sampling instantly, then PPI handles it via Timer CC
@@ -61,7 +60,7 @@ void stop_heating(void)
     nrf_gpio_pin_set(RED_LED);        // => OFF
     timer_for_saadc_sampling_stop();
 
-    k_msleep(5000);
+    k_msleep(TURN_ON_INTERVAL_MS);
 
     start_heating();
 }
@@ -78,21 +77,21 @@ int main(void)
     saadc_init(saadc_handler);
     ppi_init();
 
-    k_sem_take(&saadc_sem, K_FOREVER);
+    k_sem_take(&saadc_ready_sem, K_FOREVER); // wait unti SAADC is ready
     start_heating();
 
     for (;;)
     {
-        k_sem_take(&saadc_sem, K_FOREVER);
+        k_sem_take(&saadc_done_sem, K_FOREVER);
 
         vddh_div5_raw    = (int32_t)saadc_results[0];
         coil_voltage_raw = (int32_t)saadc_results[1];
 
         coil_power_raw = vddh_div5_raw * coil_voltage_raw;
 
-        LOG_INF("current saadc bufffer address: 0x%x result %d ", (uint32_t)saadc_results, coil_power_raw);
+        LOG_INF("%d", coil_power_raw);
 
-        if (coil_power_raw > 200000)
+        if (coil_power_raw > 300000)
         {
             stop_heating();
         }
